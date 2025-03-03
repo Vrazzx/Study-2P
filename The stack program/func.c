6fa33671a9130dc9835212f04f575a4b4e7c196d
Belogrivtsev <nuclearthrone55@gmail.com>
2025-03-03 15:38:28 +0200

    feat:выложен  нерабочий стэк

---
+++ The stack program/func.c
@@ -0,0 +1,5 @@
+#include "func.h"
+void add(int stack[]) {
+    //int a = sizeof(stack);
+    printf(10);
+}
---
+++ The stack program/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "func.h"
+
+int main() {
+    int stack[3];
+    stack[0] = 1;
+    stack[1] = 2;
+    //add(stack);
+}
