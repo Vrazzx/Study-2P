4d2ae92530cca3a1a0dcdc74d2ad84bc650eb0aa
Vrazzx <ivan.vraz@gmail.com>
2025-03-10 10:49:27 +0600

    update

--- Lab1-Task1/roots_function.c
+++ Lab1-Task1/roots_function.c
@@ -3,26 +3,26 @@
 
 void find_roots(double a, double b, double c, double roots[2], int *root_count,
                 double epsilon) {
-  //Если а ноль или меньше эпсилон корней нет
+  // Если а ноль или меньше эпсилон корней нет
   if (a == 0 || a < epsilon) {
     *root_count = 0;
     return;
   }
-  //Если абс значение б или с меньше эпсилон зануляем
+  // Если абс значение б или с меньше эпсилон зануляем
   if (fabs(b) < epsilon) {
     b = 0;
   }
   if (fabs(c) < epsilon) {
     c = 0;
   }
-  //Дискрименант
+  // Дискрименант
   double discrimenant = (b * b) - 4 * a * c;
   if (sqrt(discrimenant) < epsilon) {
     discrimenant = 0;
   }
-  
+
   if (discrimenant > 0) {
-    //Применяем формулу Виетта если а = 1
+    // Применяем формулу Виетта если а = 1
     if (a == 1) {
       if (b > 0) {
         roots[0] = (-b - sqrt(discrimenant)) / (2 * a);
@@ -31,20 +31,20 @@ void find_roots(double a, double b, double c, double roots[2], int *root_count,
         roots[1] = (-b + sqrt(discrimenant)) / (2 * a);
         roots[0] = c / (a * roots[1]);
       }
-      //Иначе обычная формула
+      // Иначе обычная формула
     } else {
       roots[0] = (-b + sqrt(discrimenant)) / 2 * a;
       roots[1] = (-b - sqrt(discrimenant)) / 2 * a;
     }
 
     *root_count = 2;
-    //Сортируем корни
+    // Сортируем корни
     if (roots[0] > roots[1]) {
       double temp = roots[0];
       roots[0] = roots[1];
       roots[1] = temp;
     }
-    //Случай дискрименанта = 0
+    // Случай дискрименанта = 0
   } else if (discrimenant == 0) {
     roots[0] = -b / (2 * a);
     *root_count = 1;
--- Makefile
+++ Makefile
@@ -4,12 +4,12 @@ check_fmt:
 	find . -regex '.*\.[ch]' -exec clang-format -style=LLVM --dry-run --Werror {} +
 
 run:
-  	find . -name "Makefile" -execdir make -f {} \;
-  	if find . -type f -name "*_test" -exec sh -c '{} && echo "тест пройден успешно"' \; ; then \
+	find . -name "Makefile" -execdir make -f {} \;
+	if find . -type f -name "*_test" -exec sh -c '{} && echo "тест пройден успешно"' \; ; then \
     	echo "Все тесты выполнены."; \
-  	else \
+  else \
     	echo "Тесты не найдены."; \
-  	fi
+  fi
 
 clean:
 	find . -type f \( -name "*.o" -o -name "*.a" -o -name "*_test" \) -exec rm -f {} +
