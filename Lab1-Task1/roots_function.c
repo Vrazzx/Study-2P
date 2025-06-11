38c7b0d44fc6056d596319ad740e1a228e0a225b
Vrazzx <ivan.vraz@gmail.com>
2025-06-11 22:33:11 +0600

    Пересмотренна функция find_roots. внесены изменения

--- Lab1-Task1/roots_function.c
+++ Lab1-Task1/roots_function.c
@@ -10,35 +10,29 @@ static void sort_roots(double *root1, double *root2) {
 
 void find_roots(double a, double b, double c, double *roots, int *root_count,
                 double epsilon) {
-
   if (fabs(a) <= epsilon) {
     *root_count = 0;
     return;
   }
 
-  double discriminant = (b * b) - 4 * a * c;
+  double discriminant = b * b - 4 * a * c;
+
   if (sqrt(discriminant) <= epsilon) {
     discriminant = 0;
   }
 
   if (discriminant > epsilon) {
+    double sqrt_discr = sqrt(discriminant);
 
-    if (fabs(a - 1.0) < epsilon * fmax(1.0, fabs(a))) {
-      if (b > epsilon) {
-        roots[0] = (-b - sqrt(discriminant)) / (2 * a);
-        roots[1] = c / (a * roots[0]);
-      } else {
-        roots[1] = (-b + sqrt(discriminant)) / (2 * a);
-        roots[0] = c / (a * roots[1]);
-      }
-
+    if (b >= epsilon) {
+      roots[0] = (-b - sqrt_discr) / (2 * a);
+      roots[1] = c / (a * roots[0]);
     } else {
-      roots[0] = (-b + sqrt(discriminant)) / (2 * a);
-      roots[1] = (-b - sqrt(discriminant)) / (2 * a);
+      roots[1] = (-b + sqrt_discr) / (2 * a);
+      roots[0] = c / (a * roots[1]);
     }
 
     *root_count = 2;
-
     sort_roots(&roots[0], &roots[1]);
 
   } else if (fabs(discriminant) <= epsilon) {
