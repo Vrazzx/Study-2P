45a0ef540e68cb673df3af2f0204939c7b0090ee
Vrazzx <ivan.vraz@gmail.com>
2025-06-11 17:03:16 +0600

    style: удалил лишние коментарии

--- Lab1-Task1/roots_function.c
+++ Lab1-Task1/roots_function.c
@@ -1,6 +1,5 @@
 #include <math.h>
 
-// Сортировка двух корней по возрастанию
 static void sort_roots(double *root1, double *root2) {
   if (*root1 > *root2) {
     double temp = *root1;
@@ -11,20 +10,20 @@ static void sort_roots(double *root1, double *root2) {
 
 void find_roots(double a, double b, double c, double *roots, int *root_count,
                 double epsilon) {
-  // Если а ноль или меньше эпсилон корней нет
+ 
   if (fabs(a) <= epsilon) {
     *root_count = 0;
     return;
   }
 
-  // Дискриминант
+  
   double discriminant = (b * b) - 4 * a * c;
   if (sqrt(discriminant) <= epsilon) {
     discriminant = 0;
   }
 
   if (discriminant > epsilon) {
-    // Применяем формулу Виетта если а = 1
+    
     if (fabs(a - 1.0) < epsilon) {
       if (b > 0) {
         roots[0] = (-b - sqrt(discriminant)) / (2 * a);
@@ -33,17 +32,17 @@ void find_roots(double a, double b, double c, double *roots, int *root_count,
         roots[1] = (-b + sqrt(discriminant)) / (2 * a);
         roots[0] = c / (a * roots[1]);
       }
-      // Иначе обычная формула
+     
     } else {
       roots[0] = (-b + sqrt(discriminant)) / (2 * a);
       roots[1] = (-b - sqrt(discriminant)) / (2 * a);
     }
 
     *root_count = 2;
-    // Сортируем корни
+    
     sort_roots(&roots[0], &roots[1]);
 
-    // Случай дискриминанта = 0
+   
   } else if (fabs(discriminant) <= epsilon) {
     roots[0] = -b / (2 * a);
     *root_count = 1;
